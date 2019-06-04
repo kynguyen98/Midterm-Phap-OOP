a9c4a6f0d50da075ade80166beb885c5a817aa0e
ky <nguyenhongky1998@gmail.com>
2019-06-04 01:58:36 -0700

    Fully Functional

--- project.java
+++ project.java
@@ -4,230 +4,336 @@ import java.awt.event.*;
 import java.awt.*;
 import javax.swing.table.DefaultTableModel;
 import static javax.swing.JFrame.EXIT_ON_CLOSE; 
-public class project extends Frame implements ActionListener{
-    JButton a,b,c,d,e,f;
-    String[] colums ={"ID","Name","Math","Phys","Chem","Aver"};
-	String[][] Data ;
-    Vector vData = new Vector();
-    Vector vTitle = new Vector();
+public class project extends JFrame implements ActionListener
+{
+	/*tạo bảng để chứa thông tin truy vấn từ csdl*/
+    Vector vData=new Vector();
+    Vector vTitle=new Vector();	
+    JScrollPane tableResult;
     DefaultTableModel model;
-    JScrollPane tableresult;
-    JTable tb = new JTable();
-    public void load(){
+    JTable tb= new JTable(); 
+    JPanel p1 ;
+	JButton input, exit, list;
+	
 
-        try{ 
-            //vTitle.clear(); 	  
-            //vData.clear();    
-               int num_column = colums.length;
-    
-               /*Chuẩn bị dữ liệu để tạo bảng (JTable)
-               Tạo các tên cột cho bảng*/
-               for (int i=0; i<num_column;i++) 
-               {	    	   
-                   vTitle.add(colums[i]);
-               }
-               
-                /*Tạo dữ liệu các hàng cho bảng: 
-                 * mỗi phần tử của Vector vData là một Vector */
-               for (int i=0; i<Data.length;i++) 
-               {
-                   Vector row = new Vector(num_column);
-                   for (int j=0; j<num_column;j++)  
-                             row.add(Data[i][j]);
-                vData.add(row);
-               }
-               
-    
-               }catch(Exception e)
-                {
-                   System.out.println(e.getMessage());
-               }
+	
+	/*Du lieu khoi tao	  */
+	String[] colums ={"ID","Name","Math","Phys","Chem","Aver"};
+	String[][] Data ;
+	
+	
+	public  project(String s)
+	{
+		super(s);
+  
+
+JFrame f = new JFrame("Student Management");
+p1 = new JPanel();
+p1.setBounds(10,150,100,100);
+p1.setBackground(Color.white);
+input = new JButton("Input");
+input.addActionListener(this);
+list = new JButton("List");
+list.addActionListener(this);
+exit = new JButton("Exit");
+exit.addActionListener(this);
+
+p1.add(input);
+p1.add(list);
+p1.add(exit);
+load();
+f.add(p1);
+f.setSize(400,300);
+f.setLayout(null);    
+f.setVisible(true);
+f.getContentPane().setBackground(Color.gray);
+
+	}
+	public void load()
+	{
+		  try{
+			
+			  
+		/*Xóa hết dữ liệu hiện có trong 2 vector*/
+		 vTitle.clear(); 	  
+	        vData.clear();  
+	       
+
+	       int num_column = colums.length;
+
+	       /*Chuẩn bị dữ liệu để tạo bảng (JTable)
+	       Tạo các tên cột cho bảng*/
+	       for (int i=0; i<num_column;i++) 
+	       {	    	   
+	       	vTitle.add(colums[i]);
+	       }
+	       
+	        /*Tạo dữ liệu các hàng cho bảng: 
+	         * mỗi phần tử của Vector vData là một Vector */
+	       for (int i=0; i<Data.length;i++) 
+	       {
+	       	Vector row = new Vector(num_column);
+	       	for (int j=0; j<num_column;j++)  
+	       	  		row.add(Data[i][j]);
+	        vData.add(row);
+	       }
+	       
+
+	       }catch(Exception e)
+	        {
+	       	System.out.println(e.getMessage());
+	       }
+	
+	}
+
+    /*Chen dữ liệu vào danh sách từ cửa sổ nhập và soạn thảo */
+	public void insertList(String id, String name, float math, float phys, float chem, float aver)
+	{
+	    try{
+	    	Vector row = new Vector();
+	    	row.add(id);
+	    	row.add(name);
+	    	row.add(math+"");
+	    	row.add(phys+"");
+	    	row.add(chem+"");
+	    	row.add(aver+"");
+		/*them doi tuong vao vData*/
+		 vData.add(row);
+		 
+	/*Cập nhật lại nội dung bảng hiển thị trên màn hình*/
+		 model.fireTableDataChanged();
+		
+	    }catch(Exception e)
+	    {
+	    	e.printStackTrace();
+	    }
+	}
+	
+/*Cập nhật dữ liệu vào danh sách từ cửa sổ nhập và soạn thảo */
+	public void editList(String id, String name, float math, float phys, float chem, float aver)
+	{
+	    try{
+	    	
+	    	Enumeration e = vData.elements();
+	    	int i=0;
+	    	
+	    	while (e.hasMoreElements())
+	    	{
+	    		Vector st = (Vector)e.nextElement();
+	    		if (st.elementAt(0).equals(id)) 
+	    		{
+	    			vData.remove(i);
+	    		    break;
+	    		}
+	    			i++;
+	    	}
+	    	Vector row = new Vector();
+	    	row.add(id);
+	    	row.add(name);
+	    	row.add(math+"");
+	    	row.add(phys+"");
+	    	row.add(chem+"");
+	    	row.add(aver+"");
+		/*Xóa nội dung hàng tương ứng trong vData*/
+		 vData.add(i,row);
+		 
+		 /*Cập nhật lại nội dung bảng hiển thị trên màn hình*/
+		 model.fireTableDataChanged();
+		
+	    }catch(Exception e)
+	    {
+	    	e.printStackTrace();
+	    }
+	}
+	
+	/* Xóa dữ liệu khi người dùng chọn hàng và ấn nút Delete */
+
+
+/*Xử lý khi người dùng ấn các nút trên cửa số  */
+public void actionPerformed(ActionEvent e)
+{
+	if(e.getActionCommand().equals("Input"))
+	{
+		new UpdateForm("Insert form",this,"","","0","0","0");
+    }	
+    if (e.getActionCommand().equals("List")){
+        new list();
     }
-    public project(){
-        this.setTitle("Student Management");
-        this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
-
-        a = new JButton("Input Student");
-        a.addActionListener(this);
-        this.add(a);
-        
-        b = new JButton("List Student");
-        b.addActionListener(this);
-        this.add(b);
-        
-        c = new JButton ("Sort by average accending");
-        c.addActionListener(this);
-        this.add(c);
-        
-        d = new JButton ("Find student name");
-        d.addActionListener(this);
-        this.add(d);
-        
-        e = new JButton ("Student who have scholarship");
-        e.addActionListener(this);
-        this.add(e);
-        
-        f = new JButton ("Exit");
-        f.addActionListener(this);
-        this.add(f);
-
-        this.setSize(300,300);
-        this.show();
+    if (e.getActionCommand().equals("Exit")){
+        System.exit(0);
     }
 
-    public static void main(String[] args) {
-        Frame k =new Frame("Student Management");
-        project st = new project();
-        
+}
+
+
+	public static void main(String[] args) {
+    	new project("Student Management");
+    }
+    class list extends JFrame implements ActionListener{
+        JButton back;
+        public list(){
+    model = new DefaultTableModel(vData,vTitle);		  
+    tb=new JTable(model); 
+    
+    tableResult = new JScrollPane(tb);
+    back = new JButton("Back");
+    back.addActionListener(this);
+    this.add(back,"South");
+    this.getContentPane().add(tableResult,"North");    
+    this.setSize(400,300);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setVisible(true);
     }
     public void actionPerformed (ActionEvent e){
-        if (e.getActionCommand().equals("Input Student")){
-            new inputinterface1();
-        }
-        else if (e.getSource()==b){
-            load();
-            new list();
-        }
-        else if (e.getSource()==f){
-            System.exit(0);
-        }
-        }
-
-    class list extends JFrame implements ActionListener{
-        JButton exit;
-        JPanel p = new JPanel(); 
-        public list(){
-        
-        model = new DefaultTableModel(vData,vTitle);
-        tb = new JTable(model);
-        tableresult = new JScrollPane(tb);
-        this.setSize(300,300);
-        //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        exit = new JButton ("Exit");
-        //setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        //exit.ActionListener(this);
-        this.getContentPane().add(tableresult,"North");
-        p.add(exit,"South");
-        this.show();
-        }
-        public void actionPerformed(ActionEvent e){
-            if (e.getActionCommand().equals("Exit")){
-                this.dispose();                
-            }
+        if (e.getActionCommand().equals("Back")){
+            this.dispose();
         }
     }
-    class inputinterface1 extends Frame implements ActionListener{
-        project st;
-        Label lb1,lb2,lb3,lb4,lb5,lb6,lb7;
-        TextField tx1,tx2,tx3,tx4,tx5;
-        Button a,b;
-        int id;
-        float math,chem,phy,avg;
-        String name;
-        
-
-        public inputinterface1(){
-            this.setTitle("Input Student");
-            this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
-            
-            lb1 = new Label("Id");
-            tx1 = new TextField();
-            this.add(lb1);
-            this.add(tx1);
-    
-            lb2 = new Label("Name");
-            tx2 = new TextField();
-    
-            this.add(lb2);
-            this.add(tx2);
-    
-            lb3 = new Label ("Math");
-            tx3 = new TextField();
-    
-            this.add(lb3);
-            this.add(tx3);
-    
-            lb4= new Label ("Chemistry");
-            tx4 = new TextField();
-    
-            this.add(lb4);
-            this.add(tx4);
-    
-            lb5 = new Label("Physic");
-            tx5 = new TextField();
-            this.add(lb5);
-            this.add(tx5);
-
-            a = new Button ("Submit");
-            a.addActionListener(this);
-            this.add(a);
-    
-            b = new Button ("Back");
-            b.addActionListener(this);
-            this.add(b);
-    
-            lb6 = new Label("");
-            lb7 = new Label("");
-            lb6.setVisible(false);
-            lb7.setVisible(false);
-            this.add(lb6);
-            this.add(lb7);
-            
-
-
-            this.pack();
-            this.setSize(300,300);
-            this.show();
-    
-        }
-        public void actionPerformed(ActionEvent e){
-            if (e.getSource()==a){
-                inputdb();
-            }
-            if (e.getSource()==b){
-                this.dispose();
-            }
-        }
-        public void inputdb(){
-            if (tx1.getText().equals("")||tx2.getText().equals("")||tx3.getText().equals("")||tx4.getText().equals("")||tx5.getText().equals("")){
-                lb6.setText("Error");
-                lb7.setText("Empty value");
-                lb6.setForeground(Color.RED);
-                lb7.setForeground(Color.RED);
-                this.setVisible(true);
-                this.setVisible(true);
-            }
-            else {
-                try{
-                    int id = Integer.parseInt(tx1.getText());
-                    String name = tx2.getText();
-                    Float m = Float.parseFloat(tx3.getText());
-                    Float c = Float.parseFloat(tx4.getText());
-                    Float p = Float.parseFloat(tx5.getText());
-                    insertlist(id,name,m,c,p,(m+c+p)/3);
-                }
-                catch(Exception e){
-                    e.printStackTrace();
-                }
-            }
-        }
-        public void insertlist(int id,String name,float math,float Chemistry,float Physic,float avg){
-            try {
-                Vector row = new Vector();
-                row.add(id);
-                row.add(name);
-                row.add(math);
-                row.add(Chemistry);
-                row.add(Physic);
-                row.add(avg);
-                vData.add(row);
-                model.fireTableDataChanged();
-            } catch (Exception e) {
-                //TODO: handle exception
-                e.printStackTrace();
-            }
-            }
-    
     }
-    
+    class UpdateForm extends JFrame implements ActionListener
+{
+   //các thành phần trên giao diện
+	JLabel Idlb; 
+	JTextField Id; 
+	JLabel namelb; 
+	JTextField name; 
+	JLabel mathlb;
+	JTextField math;
+	JLabel physlb;
+	JTextField phys;
+	JLabel chemlb;
+	JTextField chem;
+	
+	JLabel errorlb;
+	JLabel errordetails;	
+	
+	JButton ok;
+	JButton cancel;	
+	
+	project mfr;
+
+
+
+public UpdateForm(String title,  project mf, String id, String na, String m, String ph, String ch)
+{
+	super(title);
+    mfr =mf;
+	Container cont = this.getContentPane();
+	cont.setLayout(new GridLayout(7,2));
+
+	Idlb = new JLabel("Id");
+	Id = new JTextField(id);
+	cont.add(Idlb);
+	cont.add(Id);	
+	if (title.equals("Edit form")) Id.setEditable(false);
+	
+	namelb = new JLabel("Name");
+	name = new JTextField(na);
+	cont.add(namelb);
+	cont.add(name);
+	
+	mathlb= new JLabel("Math");
+	math= new JTextField(m);
+	cont.add(mathlb);
+	cont.add(math);
+	
+	physlb= new JLabel("Physics");
+	phys= new JTextField(ph);
+	cont.add(physlb);
+	cont.add(phys);
+	
+	chemlb= new JLabel("Chemistry");
+	chem= new JTextField(ch);	
+	cont.add(chemlb);
+	cont.add(chem);
+
+	errorlb= new JLabel("");
+	errordetails= new JLabel("");	
+	errorlb.setVisible(false);
+	errordetails.setVisible(false);
+	cont.add(errorlb);
+	cont.add(errordetails);
+	
+	JButton ok = new JButton("Ok");
+	JButton cancel = new JButton("Cancel");	
+	cont.add(ok);
+	cont.add(cancel);	
+	ok.addActionListener(this);
+	cancel.addActionListener(this);
+
+	this.setSize(230,200);
+    this.setLocation(250, 100);
+    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    this.setVisible(true);
+      
+
+}
+
+public void actionPerformed(ActionEvent e)
+{
+	if(e.getActionCommand().equals("Ok"))
+	{
+        //insertDB();
+        	    String id = Id.getText();		 
+        String na = name.getText();
+		float m =Float.parseFloat(math.getText());
+		float ph =Float.parseFloat(phys.getText());
+		float ch =Float.parseFloat(chem.getText());	
+        mfr.insertList(id,na,m,ph,ch,(m+ph+ch)/3);
     }
+	else this.dispose();
+	
+	
+}
+public void insertDB()
+{
+if ( Id.getText().equals("")||name.getText().equals("")||math.getText().equals("")||phys.getText().equals("")||chem.getText().equals(""))
+	{
+		/*Tạo nội dung lỗi */
+		errorlb.setText("Error");
+		errordetails.setText("empty value");			
+		errorlb.setForeground(Color.RED);
+		errordetails.setForeground(Color.RED);
+		
+		/*Hiển thị lỗi*/
+		errorlb.setVisible(true);
+		errordetails.setVisible(true);
+	}
+	else
+	{
+		
+	 try{
+		//Lấy nội dung đã nhập ở giao diện
+	    String id = Id.getText();		 
+        String na = name.getText();
+		float m =Float.parseFloat(math.getText());
+		float ph =Float.parseFloat(phys.getText());
+		float ch =Float.parseFloat(chem.getText());	
+		
+		//cập nhật giao diện cửa sổ chính	
+		if (this.getTitle().equals("Insert form"))
+			mfr.insertList(id,na,m,ph,ch,(m+ph+ch)/3);
+		else
+        mfr.editList(id,na,m,ph,ch,(m+ph+ch)/3);
+	
+		
+
+	}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+	}
+}
+}
+
+}
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
